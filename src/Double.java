306c0fd5ac02d1fda2d04c37d0e57fe76b7c3481
Jean <jeanlucorrrea@gmail.com>
2022-10-09 17:00:22 -0300

    POO aula 1

---
+++ src/Double.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Double {
+
+	public static void main(String[] args) {
+		// Programa para calcular o dobro de um número
+		
+		
+		// Vamos ativar a entrada do sistema
+		Scanner entrada = new Scanner(System.in);
+		
+		// Vamos pedir para o usuário digitar um valor
+		// ATALHO SystemOut => syso ctrl + espaço
+		
+		System.out.println("Digite um número: ");
+		
+		// Vamos criar uma variável para guardar o valor
+		
+		double valor = entrada.nextDouble();
+		
+		// Vamos mostrar o dobro desse número
+		
+		System.out.println("O dobro é:" + 2*valor);
+		
+		entrada.close();
+
+	}
+
+}
